83b0599e1d361a480d92758abf04a29ba8b9b351
Lukas <lukas_hecker@web.de>
2020-09-23 17:30:42 +0200

    interesting, go on there

--- Plotting.py
+++ Plotting.py
@@ -31,7 +31,7 @@ data = np.random.normal(size=(100,1000))
 ptr = 0
 def update():
     global curve, data, ptr, p6
-    curve.setData(data[ptr%10])
+    curve.setData(data[ptr%10, :])
     if ptr == 0:
         p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
     ptr += 1
@@ -44,6 +44,26 @@ timer = QtCore.QTimer()
 timer.timeout.connect(update) 
 timer.start(50) # interval at which the plot will be updated (update function will be called every 50 ms)
 
+
+p7 = win.addPlot(title="Updating plot")
+curve7 = p7.plot(pen='r')
+data7 = np.random.normal(size=(100,1000))
+ptr7 = 0
+def update2():
+    print("i")
+    global curve7, data7, ptr7, p7
+    curve7.setData(data[ptr7%10, :])
+    if ptr7 == 0:
+        p7.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    ptr7 += 1
+
+
+# The QTimer class provides a high-level programming interface for timers. To use it, create
+# a QTimer, connect its timeout() signal to the appropriate slots, and call start(). 
+# From then on, it will emit the timeout() signal at constant intervals.
+timer2 = QtCore.QTimer()
+timer2.timeout.connect(update2) 
+timer2.start(50) # interval at which the plot will be updated (update function will be called every 50 ms)
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
